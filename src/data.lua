dae7c9448cebe9e8a4dfbd3f3509d80e9c496434
Daniel Brooks <db48x@db48x.net>
2015-10-04 08:49:52 -0700

    three tracks and the prototypes that tell the game to play them

---
+++ src/data.lua
@@ -0,0 +1,30 @@
+data:extend(
+{
+  {
+    type = "ambient-sound",
+    name = "Original Factorio Intro (HD)",
+    track_type = "early-game",
+    sound =
+    {
+      filename = "__original-music-hd__/FactorioIntro.ogg"
+    }
+  },
+  {
+    type = "ambient-sound",
+    name = "Original Factorio Haluzsqua (HD)",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__original-music-hd__/FactorioHaluzsqua.ogg"
+    }
+  },
+  {
+    type = "ambient-sound",
+    name = "Original Factorio Sepro1 (HD)",
+    track_type = "main-track",
+    sound =
+    {
+      filename = "__original-music-hd__/FactorioSepro1.ogg"
+    }
+  }
+})
